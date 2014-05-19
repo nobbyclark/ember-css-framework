adefd6f265a4fbafa6f7b5676b1e0de90aee24f9
Nick Clark <nick@carnivallabs.com>
2014-05-20 10:25:40 +1200

    Added transition to height of reveal

--- app/scripts/views/reveal_view.js
+++ app/scripts/views/reveal_view.js
@@ -3,6 +3,18 @@ App.RevealView = Ember.View.extend({
   classNames: ['reveal'],
   classNameBindings: ['isOpen:open'],
 
+  onToggleReveal: function() {
+
+    var el = this.$('.reveal-content');
+
+    if (this.get('isOpen')) {
+      el.css('height', this.$('.reveal-content-wrap').outerHeight());
+    } else {
+      el.css('height', "");
+    }
+
+  }.observes('isOpen'),
+
   actions: {
     toggleReveal: function() {
       this.toggleProperty('isOpen');
