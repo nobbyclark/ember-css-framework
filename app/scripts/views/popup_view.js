dfdd790d0ae2db32db606d5626ac96613832ab21
Nick Clark <nick@carnivallabs.com>
2014-05-20 15:16:19 +1200

    Added logic for positioning popup for small toggles

--- app/scripts/views/popup_view.js
+++ app/scripts/views/popup_view.js
@@ -2,7 +2,6 @@ App.PopupView = App.DropdownView.extend({
   classNames: ['reveal-popup'],
   offset: 10,
   position: 'top-center',
-  hasArrow: true,
 
   onToggleReveal: function() {
     
@@ -17,26 +16,31 @@ App.PopupView = App.DropdownView.extend({
         contentWidth = 0,
         contentHeight = 0,
 
-        arrowOffset = 12,
-        arrowWidth = 24,
+        arrow = this.$('.reveal-content > .arrow'),
+        arrowOffset = 0,
+        arrowWidth = 0,
+        arrowHeight = 0,
 
+        // distance from toggle
         offset = this.get('offset'),
+
         position = this.get('position').split('-'),
         a = position[0],
         b = position[1],
         xPos,
         yPos;
 
-    if (this.get('hasArrow')) {
-      content.addClass('arrow arrow-' + a + ' ' + b);
-    }
+    // Add correct arrow class
+    content.addClass('arrow-' + a + ' ' + b);
 
-    // get height AFTER hasArrow check otherwise
-    // height wont take arrows height into account
+    // get height AFTER arrow check otherwise
+    // height wont take arrow height into account
     contentWidth = content.outerWidth();
     contentHeight = content.outerHeight();
 
-    // Determine positon
+    arrowWidth = arrow.outerWidth();
+    arrowHeight = arrow.outerHeight();
+
     if (a === 'top' || a === 'bottom') {
 
       if (a === 'top') {
@@ -49,8 +53,20 @@ App.PopupView = App.DropdownView.extend({
         xPos = -((contentWidth - toggleWidth) / 2);
       } else if (b === 'left') {
         xPos = 0;
+
+        if (toggleWidth < contentWidth) {
+          arrowOffset = arrow.position().left;
+          xPos -= (arrowOffset + ((arrowWidth / 2) - (toggleWidth / 2)));
+        }
+
       } else if (b === 'right') {
         xPos = toggleWidth - contentWidth;
+
+        if (toggleWidth < contentWidth) {
+          arrowOffset = contentWidth - arrow.position().left - arrowWidth;
+          xPos += (arrowOffset + ((arrowWidth / 2) - (toggleWidth / 2)));
+        }
+
       }
 
     }
@@ -66,9 +82,21 @@ App.PopupView = App.DropdownView.extend({
       if (b === 'center') {
         yPos = -((contentHeight - toggleHeight) / 2);
       } else if (b === 'top') {
-        yPos = 0;// - (arrowOffset + ((arrowWidth / 2) - (toggleHeight / 2)));
+        yPos = 0;
+
+        if (toggleHeight < contentHeight) {
+          arrowOffset = arrow.position().top;
+          yPos -= (arrowOffset + ((arrowHeight / 2) - (toggleHeight / 2)));
+        }
+
       } else if (b === 'bottom') {
         yPos = toggleHeight - contentHeight;
+
+        if (toggleHeight < contentHeight) {
+          arrowOffset = contentHeight - arrow.position().top - arrowHeight;
+          yPos += (arrowOffset + ((arrowHeight / 2) - (toggleHeight / 2)));
+        }
+        
       }
       
     }
