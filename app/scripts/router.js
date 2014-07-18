442f59a94d937bd7791c3cc53e93a34eed328efb
Nick Clark <nick@carnivallabs.com>
2014-07-18 16:09:14 +1200

    Added 'Icons' page

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,6 +2,7 @@ App.Router.map(function () {
   this.route('type');
   this.route('buttons');
   this.route('forms');
+  this.route('icons');
   this.resource('reveals', function() {
     this.route('panels');
     this.route('dropdowns');
